ed27b71a6981e7acdc96845c85d8e5bc9a6dc206
Aleksandr <sasha.syrota15@gmail.com>
2025-07-20 16:34:57 +0300

    implement ordering by create datetime

--- todo/models.py
+++ todo/models.py
@@ -14,3 +14,6 @@ class Task(models.Model):
     deadline = models.DateTimeField(null=True)
     done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name='tasks')
+
+    class Meta:
+        ordering = ['-created_at']
