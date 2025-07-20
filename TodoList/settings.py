b48d2eb8ba73dba7e1e8ed958b0aefbdda8a240f
Aleksandr <sasha.syrota15@gmail.com>
2025-07-20 16:28:42 +0300

    add flake8

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -15,18 +15,18 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3mg_t00n^bg(3)h5e-l&gmejrbh!73_jn5w+705co3!&u0ula_'
+SECRET_KEY = \
+    'django-insecure-3mg_t00n^bg(3)h5e-l&gmejrbh!73_jn5w+705co3!&u0ula_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'crispy_forms',
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -69,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TodoList.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -80,25 +81,28 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -110,7 +114,6 @@ TIME_ZONE = 'Europe/Kiev'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
@@ -125,3 +128,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- todo/urls.py
+++ todo/urls.py
@@ -4,14 +4,30 @@ from todo import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("task_complete/<int:pk>", views.task_complete_view, name='task_complete'),
+    path(
+        "task_complete/<int:pk>",
+        views.task_complete_view,
+        name='task_complete'
+    ),
     path("task_create/", views.TaskCreateView.as_view(), name='task_create'),
-    path("task_update/<int:pk>", views.TaskUpdateView.as_view(), name='task_update'),
-    path("task_delete/<int:pk>", views.TaskDeleteView.as_view(), name='task_delete'),
+    path(
+        "task_update/<int:pk>",
+        views.TaskUpdateView.as_view(),
+        name='task_update'
+    ),
+    path(
+        "task_delete/<int:pk>",
+        views.TaskDeleteView.as_view(),
+        name='task_delete'
+    ),
     path("tags/", views.TagsListView.as_view(), name='tags_list'),
     path("tag_create/", views.TagCreateView.as_view(), name='tag_create'),
-    path("tag_update/<int:pk>", views.TagUpdateView.as_view(), name='tag_update'),
-    path("tag_delete/<int:pk>", views.TagDeleteView.as_view(), name='tag_delete'),
+    path(
+        "tag_update/<int:pk>", views.TagUpdateView.as_view(), name='tag_update'
+    ),
+    path(
+        "tag_delete/<int:pk>", views.TagDeleteView.as_view(), name='tag_delete'
+    ),
 ]
 
 app_name = "todo"
